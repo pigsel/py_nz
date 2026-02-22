45d011bff3ad5b5731089f99305ff29cce1f7b6b
Igor <bertyaev@gmail.com>
2026-02-23 12:51:25 +1300

    note for xml updated

---
+++ SSR_PowerBI/PowerBI_extract.py
@@ -0,0 +1,6 @@
+# this script extracts data from PowerBI reports and saves them as XLS files.
+# For TP SSR Reports
+
+
+from pathlib import Path
+import pandas as pd
--- parse_str/StkTableParser.py
+++
@@ -1,7 +0,0 @@
-# Staking table reader / parser
-# Igor Bertyaev, 2025
-
-
-import pandas as pd
-from pathlib import Path    
-
